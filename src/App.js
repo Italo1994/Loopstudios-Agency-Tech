dfa531e7debc78547bccc11e40dd5944bb71b9ef
Italo1994 <italohortiz2015@gmail.com>
2023-01-09 22:58:29 -0300

    Create component Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header.js';
 import Banner from './components/Banner/Banner.js';
 import Section from './components/Section/Section.js';
 import Products from './components/Products/Products.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Section titleInformation="The Leader in Interactive VR" 
         textInformation="Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand." />
       <Products />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import styles from './Footer.module.css';
+import stylesHeader from '../Header/Header.module.css';
+
+import iconFacebook from '../../assets/images/icon-facebook.svg';
+import iconTwitter from '../../assets/images/icon-twitter.svg';
+import iconPinterest from '../../assets/images/icon-pinterest.svg';
+import iconInstagram from '../../assets/images/icon-instagram.svg';
+
+import Image from '../Image/Image.js';
+
+function Footer() {
+	return(
+		<div className={styles.footerContainer}>
+			<div className={styles.footerContent}>
+				<div className={styles.footerMenuGrid}>
+					<a href="#" className={stylesHeader.logo}>loopstudios</a>
+					<ul className={stylesHeader.menuNav}>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}>About</a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}>Careers</a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}>Events</a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}>Products</a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}>Support</a></li>
+					</ul>
+				</div>
+				<div className={styles.footerSocialGrid}>
+					<ul className={stylesHeader.menuNav}>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}> <Image src={iconFacebook} alt="icon facebook" width={100} height={68} /> </a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}> <Image src={iconTwitter} alt="icon twitter" width={100} height={68} /> </a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}> <Image src={iconPinterest} alt="icon pinterest" width={100} height={68} /> </a></li>
+						<li className={stylesHeader.menuItem}><a href="#" className={stylesHeader.menuLink}> <Image src={iconInstagram} alt="icon instagram" width={100} height={68} /> </a></li>
+					</ul>
+					<p>&#169; 2023 loopstudios. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
