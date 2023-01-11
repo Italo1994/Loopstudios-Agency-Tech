1da1568d8ceba52a2a66470aa364b8c0ae2c0659
Italo1994 <italohortiz2015@gmail.com>
2023-01-11 16:14:31 -0300

    modifying the container of the components Header and Banner. Including a responsive design for the Header and Footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,35 @@
 import styles from './Header.module.css';
 
+import $ from 'jquery';
+
+import Image from '../Image/Image.js';
+
+import iconHamburger from '../../assets/images/icon-hamburger.svg';
+
 function Header() {
+	$(document).ready(() => {
+		$("#buttonMob").click(() => {
+			$("#menu").slideToggle("slow");
+		});
+	});
+
 	return(
 		<header className={styles.headerContainer}>
 			<div className={styles.headerContent}>
 				<a href="#" className={styles.logo}>loopstudios</a>
 				<ul className={styles.menuNav}>
+					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>About</a></li>
+					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>Careers</a></li>
+					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>Events</a></li>
+					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>Products</a></li>
+					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>Support</a></li>
+				</ul>
+
+				<button type="button" className={styles.buttonMobile} id="buttonMob">
+					<Image src={iconHamburger} alt="button mobile" width={100} height={68} />
+				</button>
+
+				<ul className={styles.menuNavMobile} id="menu">
 					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>About</a></li>
 					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>Careers</a></li>
 					<li className={styles.menuItem}><a href="#" className={styles.menuLink}>Events</a></li>
